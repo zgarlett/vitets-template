d3976b28dcee083d5c5b12ffbc26bd54311ea544
zgarlett <snacpacc@gmail.com>
2026-03-28 23:36:50 -0500

    refactor: remove Redux and DnD dependencies, update App component
    - Deleted index.html as it is no longer needed.
    - Removed @dnd-kit/core, @reduxjs/toolkit, react-redux from package.json and package-lock.json.
    - Refactored App component to include a counter and updated UI elements.
    - Removed Board component and related files as they are no longer used.
    - Cleaned up Redux-related files including store, slices, and hooks.
    - Updated tsconfig.json to remove references to backend configurations.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.tsx'
-import { store } from './redux/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </StrictMode>,
 )
